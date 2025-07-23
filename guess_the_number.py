131693dfc86a958158e4287612745a60a234c842
cmdi4s <devvdias@gmail.com>
2025-07-22 21:33:50 -0300

    Try to guess v1

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,21 +1,29 @@
+import random
+
 def search_number(list_numbers, number):
     low = 0
     high = len(list_numbers) - 1 
+    steps = 0
 
     while low <= high:
-        mid = (low + high) / 2
+        steps += 1 
+        mid = int((low + high) / 2)
         attempt = list_numbers[mid]
 
         if attempt == number:
-            return mid
+            return mid, steps
         if attempt > number:
             high = mid - 1 
         else:
             low = mid + 1
-    return None
+    return None, steps
 
 low_range = int(input("The lowest value on the list is ? "))
 high_range = int(input("The highest value on the list is ? ")) + 1
-
 list_numbers = [i for i in range(low_range,high_range)]
-print(list_numbers)
+
+random_number = random.randint(low_range,high_range)
+result, num_steps = search_number(list_numbers,random_number)
+
+print(f"The secret number is: {random_number}")
+print(f"The result is: {result} in {num_steps} steps")
